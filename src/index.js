22d51aadedfa27f23fb6f26f8c50a248a50d1f89
andy.zhao <andy.zhao@lunarax.io>
2022-07-19 17:30:20 +0800

    feat: 预先准备-修改webpack多输出

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
-
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack', '你好!'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
