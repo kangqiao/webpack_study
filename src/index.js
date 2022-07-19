1d5287c0b0b473a705f04432569da6e88abc4eb7
andy.zhao <andy.zhao@lunarax.io>
2022-07-19 17:25:47 +0800

    feat: 回退代码

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import _ from "lodash";
-import './style.css';
-import Icon from './icon.jpeg';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
 
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
 
 function component() {
   const element = document.createElement('div');
 
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack', '你好!'], ' ');
-  element.classList.add('hello');
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
